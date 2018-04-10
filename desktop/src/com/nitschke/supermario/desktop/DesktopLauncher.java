c724ac7f1fba971222d486781671c1624be5d26d
Marius Conradi Nitschke <mariun11@uia.no>
2018-04-10 13:44:38 +0200

    Initial Commit

---
+++ desktop/src/com/nitschke/supermario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nitschke.supermario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nitschke.supermario.MarioGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioGame(), config);
+	}
+}
